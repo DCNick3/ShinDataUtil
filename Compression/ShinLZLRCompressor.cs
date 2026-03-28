07329aecc14bb5274f0b5a064d8e7a81b24bfb09
samnighty <38229465+samnighty@users.noreply.github.com>
2026-03-28 22:51:08 +0300

    implemented compression for LZLR

--- Compression/ShinLZLRCompressor.cs
+++ Compression/ShinLZLRCompressor.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 using System.Diagnostics;
 using ShinDataUtil.Common;
@@ -10,71 +7,196 @@ using System.Runtime.InteropServices;
 
 namespace ShinDataUtil.Compression
 {
-    class ShinLZLRCompressor
+    public class ShinLZLRCompressor
     {
-        private MemoryStream mapStream = new MemoryStream();
-        private MemoryStream dataStream = new MemoryStream();
+        private MemoryStream dictStream;
+        private MemoryStream dataStream;
+        private uint currentDictBlock;
+        private int bitsInBlock;
 
-        private uint currentMapBlock = 0;
-        private int currentBit = 0;
-        public void PushBit(bool bit)
+        private void WriteBit(bool bit)
         {
             if (bit)
             {
-                currentMapBlock |= 1u << currentBit;
+                currentDictBlock |= 1u << (31 - bitsInBlock);
             }
-            currentBit++;
-
-            if (currentBit == 32)
+            bitsInBlock++;
+            if (bitsInBlock == 32)
             {
-                mapStream.WriteByte((byte)currentMapBlock);
-                mapStream.WriteByte((byte)(currentMapBlock >> 8));
-                mapStream.WriteByte((byte)(currentMapBlock >> 16));
-                mapStream.WriteByte((byte)(currentMapBlock >> 24));
-                currentMapBlock = 0;
-                currentBit = 0;
+                FlushBlock();
             }
         }
 
-        public void PushBlock(uint block)
+        private void FlushBlock()
         {
-            dataStream.WriteByte((byte)block);
-            dataStream.WriteByte((byte)(block >> 8));
-            dataStream.WriteByte((byte)(block >> 16));
-            dataStream.WriteByte((byte)(block >> 24));
+            dictStream.WriteByte((byte)currentDictBlock);
+            dictStream.WriteByte((byte)(currentDictBlock >> 8));
+            dictStream.WriteByte((byte)(currentDictBlock >> 16));
+            dictStream.WriteByte((byte)(currentDictBlock >> 24));
+            currentDictBlock = 0;
+            bitsInBlock = 0;
         }
+
+        private void FinishBits()
+        {
+            if (bitsInBlock > 0)
+            {
+                FlushBlock();
+            }
+        }
+
+        private void WriteNumber(int offset, uint num)
+        {
+            uint bitmaskL = 1u << (offset - 1);
+            while (bitmaskL > 0)
+            {
+                WriteBit((num & bitmaskL) != 0);
+                bitmaskL >>= 1;
+            }
+        }
+
+        private void WriteSequenceCount(uint val)
+        {
+            int bitcount = 0;
+            while (val >= ((1u << (bitcount + 1)) - 1))
+            {
+                if (bitcount == 23 || val < ((1u << (bitcount + 2)) - 1))
+                {
+                    break;
+                }
+                bitcount++;
+            }
+
+            for (int i = 0; i < bitcount; i++)
+            {
+                WriteBit(true);
+            }
+            WriteBit(false);
+
+            uint offsetTableVal = (1u << (bitcount + 1)) - 1;
+            WriteNumber(bitcount + 1, val - offsetTableVal);
+        }
+
+        private void FindBestMatch(ReadOnlySpan<uint> inputWords, int pos, out uint bestBackOffset, out int bestMatchLen)
+        {
+            bestBackOffset = 0;
+            bestMatchLen = 0;
+            int windowStart = Math.Max(0, pos - 1023);
+
+            for (int candidatePos = pos - 1; candidatePos >= windowStart; candidatePos--)
+            {
+                int matchLen = 0;
+                while (pos + matchLen < inputWords.Length && inputWords[candidatePos + matchLen] == inputWords[pos + matchLen])
+                {
+                    matchLen++;
+                }
+
+                if (matchLen > bestMatchLen)
+                {
+                    bestMatchLen = matchLen;
+                    bestBackOffset = (uint)(pos - candidatePos);
+                }
+            }
+        }
+
+        private int EncodeMatches(ReadOnlySpan<uint> inputWords, int pos, uint bestBackOffset, int bestMatchLen)
+        {
+            var matches = new List<Tuple<uint, uint>>();
+            matches.Add(new Tuple<uint, uint>((uint)bestMatchLen, bestBackOffset));
+            pos += bestMatchLen;
+
+            while (pos < inputWords.Length)
+            {
+                FindBestMatch(inputWords, pos, out uint nextBackOffset, out int nextMatchLen);
+                
+                if (nextMatchLen >= 1)
+                {
+                    matches.Add(new Tuple<uint, uint>((uint)nextMatchLen, nextBackOffset));
+                    pos += nextMatchLen;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            WriteBit(true);
+            WriteSequenceCount((uint)matches.Count);
+            foreach (var match in matches)
+            {
+                WriteSequenceCount(match.Item1);
+                WriteNumber(10, match.Item2);
+            }
+            return pos;
+        }
+
+        private int EncodeLiterals(ReadOnlySpan<uint> inputWords, int pos)
+        {
+            var literals = new List<uint>();
+            literals.Add(inputWords[pos]);
+            pos++;
+
+            while (pos < inputWords.Length)
+            {
+                FindBestMatch(inputWords, pos, out _, out int matchLen);
+                if (matchLen >= 1)
+                {
+                    break;
+                }
+                literals.Add(inputWords[pos]);
+                pos++;
+            }
+
+            WriteBit(false);
+            WriteSequenceCount((uint)literals.Count);
+            foreach (uint l in literals)
+            {
+                dataStream.WriteByte((byte)l);
+                dataStream.WriteByte((byte)(l >> 8));
+                dataStream.WriteByte((byte)(l >> 16));
+                dataStream.WriteByte((byte)(l >> 24));
+            }
+            return pos;
+        }
+
         public ReadOnlySpan<byte> Compress(ReadOnlySpan<byte> data)
         {
-            // Currently I dont know if there are LZLR-compressed files that not 4-byte aligned
-            // This compression probably used only for textures
-            Trace.Assert(data.Length % 4 == 0);
-
-            mapStream = new MemoryStream();
+            dictStream = new MemoryStream();
             dataStream = new MemoryStream();
+            currentDictBlock = 0;
+            bitsInBlock = 0;
+
+            int paddedLength = (data.Length + 3) / 4 * 4;
+            var paddedData = new byte[paddedLength];
+            data.CopyTo(paddedData);
+
+            ReadOnlySpan<uint> inputWords = MemoryMarshal.Cast<byte, uint>(paddedData);
+
+            int pos = 0;
+            while (pos < inputWords.Length)
+            {
+                FindBestMatch(inputWords, pos, out uint bestBackOffset, out int bestMatchLen);
+
+                if (bestMatchLen >= 1)
+                {
+                    pos = EncodeMatches(inputWords, pos, bestBackOffset, bestMatchLen);
+                }
+                else
+                {
+                    pos = EncodeLiterals(inputWords, pos);
+                }
+            }
+
+            FinishBits();
 
             var header = new LZLRHeader();
             header.magic = LZLRHeader.DefaultMagic;
-            header.unpackedSize = data.Length; 
-
-            var intData = MemoryMarshal.Cast<byte, uint>(data);
-
-            // WIP
-            // Direct copying, compression effectiveness below than zero, but it works 
-            for (int i = 0; i < intData.Length; i++)
-            {
-                PushBit(false);
-                PushBit(false);
-                PushBit(false);
-
-                PushBlock(intData[i]);
-            }
-
-            header.dataOffset = LZLRHeader.Size + (int)mapStream.Position;
+            header.unpackedSize = data.Length;
+            header.dataOffset = LZLRHeader.Size + (int)dictStream.Position;
 
             var outStream = new MemoryStream();
-
             outStream.Write(SpanUtil.AsReadOnlyBytes(ref header));
-            outStream.Write(mapStream.ToArray());
+            outStream.Write(dictStream.ToArray());
             outStream.Write(dataStream.ToArray());
 
             return outStream.ToArray();
